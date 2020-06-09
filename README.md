884242d741149d98b3f9e23935ddc54c3be8a533
Theieyrre <crizfmtfm@gmail.com>
2020-06-09 07:14:45 +0300

    Update README and search.py

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Twitter Web Scrapping Tool with Selenium & Chrome Web Driver
 
 ## Requirements
 > Python 3 ( 3.8.3)\
-> Chrome Browser ( Version 83 )\
+> Chrome Browser ( Version 83 )
 
 ## Install
 
@@ -16,9 +16,15 @@ To install dependencies with pip without virtualenv
 > pip install -r requirements.txt
 
 ## Run 
-> python app.py github 50 test
 
-## Parameters
-  >input&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;[REQUIRED] Topic word, to seach in twitter\
-  >min&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;Minimum tweet count, default 100\
-  >output&ensp;&ensp;&ensp;&ensp;Output file name to write tsv, default\ name output.tsv
+### Search Scraping
+
+> python search.py github 50 test
+
+| Parameters   |      Description      |
+|----------|:-------------:|
+| input |  [REQUIRED] Topic word, to seach in twitter|
+| min |    Minimum tweet count  |
+| output | Output file name to write csv |
+| -b, --browser | Option to open Chrome window |
+
---
+++ followers.py
@@ -0,0 +1,98 @@
+import sys, time
+import argparse
+from decimal import Decimal
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import presence_of_element_located
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+
+import pandas as pd
+import termcolor as t
+
+# Parse Arguments and Print Help
+
+parser = argparse.ArgumentParser(
+    description='Get followers of a Twitter user', 
+    formatter_class=argparse.RawTextHelpFormatter, 
+    epilog="Example of usage:\npython app.py tobb 1000 output.csv\n"
+    )
+parser.add_argument("input", metavar="input", help="[REQUIRED] Topic word, to seach in twitter, to search more than one word add quotes around string")
+parser.add_argument("min", metavar="min", nargs="?", help="Minimum tweet count, default 100", default=100)
+parser.add_argument("output", metavar="output", nargs="?", help="Output file name to write tsv, default name output.csv", default="output.csv")
+parser.add_argument("-b", "--browser", action='store_true', help="Option to open Chrome window to view tweets")
+args = parser.parse_args()
+
+output = args.output
+if  output.find(".csv") == -1:
+    if output.find("."):
+        output = output.split(".")[0] + ".csv"
+        print(t.colored("Non csv file given as output format. Changing to " + output, "yellow"))
+    else:
+        output = output + ".csv"
+
+# Create DataFrame
+
+df = pd.DataFrame(columns = [
+    "username", "follower_name"
+])
+
+# Web Driver Options
+
+print("Creating Chrome Web Driver...", end = "\r")
+options = webdriver.ChromeOptions()
+options.binary_location = r'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe'
+if args.browser is not True:
+    options.add_argument('--headless')
+options.add_argument("--lang=en")
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+print("Creating Chrome Web Driver..." + t.colored("Done", "green"))
+
+# Start Chrome WebDriver
+
+print("Starting Chrome Web Driver...", end = "\r")
+driver = webdriver.Chrome(executable_path=r'chromedriver.exe', options=options)
+print("Starting Chrome Web Driver..." + t.colored("Done", "green"))
+url = "https://twitter.com/" + args.input + "/followers"
+print("Waiting page to open...", end = "\r")
+driver.get(url)
+wait = WebDriverWait(driver, 10)
+print("Waiting page to open..." + t.colored("Done", "green"))
+print("Scraping url  " + t.colored(url, "blue"))
+print("Waiting DOM to get ready...", end = "\r")
+wait.until(presence_of_element_located((By.CSS_SELECTOR, "div[data-testid='primaryColumn']")))
+column = driver.find_element_by_css_selector("div[data-testid='primaryColumn']")
+wait.until(presence_of_element_located((By.CSS_SELECTOR, "section[role='region']")))
+print("Waiting DOM to get ready..." + t.colored("Ready", "green"))
+
+# Scrap Followers
+
+count = 0
+last_height = driver.execute_script("return document.body.scrollHeight")
+while count <= int(args.min):
+    percent = Decimal((count / int(args.min)) * 100)
+    print("Gathering Tweets " + t.colored(str(round(percent,1)) + "%","magenta"), end="\r")
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(2)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    user_cells = column.find_elements_by_css_selector("div[data-testid='UserCell']")
+    try:
+        for user in user_cells:
+            html_a = user.find_element_by_css_selector("a[dir='auto']")
+            date = html_a.find_element_by_css_selector("time").get_attribute("datetime")
+            name_id = html_a.get_attribute("href").split("/")
+            print(name_id.get_attribute("outerHTML"))
+            is_verified = False
+            try:
+                verified = user.find_element_by_css_selector("svg[aria-label='Verified account']")
+                is_verified = True
+            except NoSuchElementException:
+                pass
+            count += 1
+            if new_height == last_height:
+                break
+            last_height = new_height
+    except StaleElementReferenceException:
+        print(t.colored("Page Structure changed !", "red"))
